3f47da2c051fda6f3f7b5c8b3c9e851db82e8376
owenmccomas <owen@mccomas.me>
2024-06-11 15:04:54 -0700

    Update env.js with server-side environment variables

--- src/env.js
+++ src/env.js
@@ -6,15 +6,15 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars.
    */
-  // server: {
+  server: {
   //   DATABASE_URL: z.string().url(),
-  //   NODE_ENV: z
-  //     .enum(["development", "test", "production"])
-  //     .default("development"),
-  //   NEXTAUTH_SECRET:
-  //     process.env.NODE_ENV === "production"
-  //       ? z.string()
-  //       : z.string().optional(),
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+    NEXTAUTH_SECRET:
+      process.env.NODE_ENV === "production"
+        ? z.string()
+        : z.string().optional(),
   //   NEXTAUTH_URL: z.preprocess(
   //     // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
   //     // Since NextAuth.js automatically uses the VERCEL_URL if present.
@@ -24,7 +24,7 @@ export const env = createEnv({
   //   ),
     // DISCORD_CLIENT_ID: z.string(),
     // DISCORD_CLIENT_SECRET: z.string(),
-  // },
+  },
 
   /**
    * Specify your client-side environment variables schema here. This way you can ensure the app
@@ -41,8 +41,8 @@ export const env = createEnv({
    */
   runtimeEnv: {
     // DATABASE_URL: process.env.DATABASE_URL,
-    // NODE_ENV: process.env.NODE_ENV,
-    // NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NODE_ENV: process.env.NODE_ENV,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     // DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
